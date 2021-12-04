862494835a3fb1499112baddf4a68871c0f8878c
Chen Hershko <3heni3@gmail.com>
2021-12-04 21:40:07 +0200

    change the website to hebrew

--- global_validations.js
+++ global_validations.js
@@ -9,7 +9,7 @@ function isNumber(num) {
 function validateLogIn() {
     let email = document.forms["login-form"]["email"].value;
     if (!isValidEmail(email)) {
-        alert("Email entered is not valid!")
+        alert("האימייל שהוכנס אינו תקין")
         return false;
     }
 }
@@ -17,17 +17,17 @@ function validateLogIn() {
 function validateRegister() {
     let email = document.forms["register-form"]["email"].value;
     if (!isValidEmail(email)) {
-        alert("Email entered is not valid!")
+        alert("האימייל שהוכנס אינו תקין")
         return false;
     }
     let fname = document.forms["register-form"]["fname"].value;
     let lname = document.forms["register-form"]["lname"].value;
     if (!validateName(fname)) {
-        alert("First name should not contain numbers, but alphabet characters only.")
+        alert("שם פרטי לא יכול להכיל מספרים, אלא אותיות בלבד")
         return false;
     }
     if (!validateName(lname)) {
-        alert("Last name should not contain numbers, but alphabet characters only.")
+        alert("שם משפחה לא יכול להכיל מספרים, אלא אותיות בלבד")
         return false;
     }
 
@@ -54,19 +54,19 @@ function isNotExpired() {
     let expire_date = document.forms["payment-form"]["expiration"].value;
     let arr = expire_date.split('/')
     if (!arr.length == 2) {
-        alert("Please write the expiration in the following form: mm/yyyy, e.g 02/2023")
+        alert("בבקשה תכתוב את תאריך פג התוקף בצורה של חח/שששש, לדוגמא 02/2023")
         return false;
     }
     let month = arr[0];
     let year = arr[1];
     if (!isNumber(month) || ! isNumber(year) || month.length != 2 || year.length != 4) {
-        alert("Month or year are not a number. Please enter 2 digits representing the month, and 4 digits representing a year, e.g 02/2023")
+        alert("החודש או השנה אינם מספר, בבבקשה הכנס 2 ספרות המייצגות את החודש ו-4 ספרות המייצגות את השנה, לדוגמא 02/2023")
         return false;
     }
     month = parseInt(month)
     year = parseInt(year)
     if (month < 0 || month > 12) {
-        alert("Month enterd is invalid. Month should be between 1-12")
+        alert("מספר החודש איננו תקין וצריך להיות בין 1-12")
         return false;
     }
     var today = new Date().toISOString().split('T'[0])[0].split('-')
@@ -77,12 +77,12 @@ function isNotExpired() {
     }
     if (year == todayYear) {
         if (month < todayMonth) {
-            alert("Credit card expiration date is already expired")
+            alert("הכרטיס אשראי פג תוקף")
             return false;
         }
     }
     if (year < todayYear) {
-        alert("Credit card expiration date is already expired")
+        alert("הכרטיס אשראי פג תוקף")
         return false;
     }
     return true;
@@ -90,21 +90,21 @@ function isNotExpired() {
 
 function isValidPayment() {
     if (!validateCreditCardNumber()) {
-        alert("Credit card is invalid. It should consist of 16 digits")
+        alert("כרטיס האשראי שהוכנס איננו תקין. כרטיס אשראי צריך להיות מורכב מ-16 ספרות")
         return false;
     }
     if (!isValidID()) {
-        alert("ID is invalid. It should consist of 9 digits")
+        alert("תעודת הזהות שהוכנסה אינה תקינה. תעודת זהות צריכה להיות מורכבת מ-9 ספרות")
         return false;
     }
     if (!isValidCVV()) {
-        alert("CVV is invalid. It should consist of 3 digits")
+        alert("שלוש הספרות בגב הכרטיס שהוכנסו אינן תקינות, אנא וודא/י כי הכנסת 3 ספרות")
         return false;
     }
     if (!isNotExpired()) {
         return false;
     }
-    alert("Payment received!")
+    alert("התשלום התקבל ונקלט במערכת!")
     return true;
 }
 function validateName(name) {
